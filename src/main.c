5ba8841a6ab5039ae3b2ed36b044a1c7149eb70d
alikolling <alikolling@gmail.com>
2021-08-31 09:04:32 -0300

    problems everywhere

--- src/main.c
+++ src/main.c
@@ -46,9 +46,10 @@ static struct k_thread t[NUM_THREADS];
 void random_gen(void *p1, void *p2, void *p3){
     
     sys_rand_get(rand, SIZE);
-    printk("Thread 1\n");
+    printk("Thread 1, estado %s\n",k_thread_state_str(&t[0]));
     k_thread_suspend(&t[0]);
     k_thread_resume(&t[1]);
+
 }
 
 void sha256(void *p1, void *p2, void *p3){
@@ -60,9 +61,9 @@ void sha256(void *p1, void *p2, void *p3){
 	tc_sha256_update(&s, (const uint8_t *)rand, SIZE);
 	(void)tc_sha256_final(digest, &s);
 	memcpy(write_buffer, digest, SIZE);
-	printk("Thread 2\n");
+	printk("Thread 2, estado %s\n",k_thread_state_str(&t[1]));
 	k_thread_suspend(&t[1]);
-    k_thread_resume(&t[2]);
+	k_thread_resume(&t[2]);
 }
 
 void write_file(void *p1, void *p2, void *p3){
@@ -70,7 +71,7 @@ void write_file(void *p1, void *p2, void *p3){
     file = open(TEST_DIR_FILE, O_RDWR);
     lseek(file, 0, SEEK_SET);
     write(file, (char *)write_buffer, SIZE);
-    printk("Thread 3\n");
+    printk("Thread 3, estado %s\n",k_thread_state_str(&t[2]));
     k_thread_suspend(&t[2]);
     k_thread_resume(&t[3]);
 
@@ -82,13 +83,14 @@ void read_file(void *p1, void *p2, void *p3){
 	size_t sz = SIZE;
     read(file, read_buff, sz);
     close(file);
-    printk("Thread 4\n");
+    printk("Thread 4, estado %s\n",k_thread_state_str(&t[3]));
     printk("%d\n", read_buff[30]);
     k_thread_runtime_stats_t rt_stats_thread;
 
     k_thread_runtime_stats_get(k_current_get(), &rt_stats_thread);
 
     printk("Cycles: %llu\n", rt_stats_thread.execution_cycles);
+    k_thread_suspend(&t[3]);
     k_thread_resume(&t[0]);
 
 }
@@ -96,23 +98,22 @@ void read_file(void *p1, void *p2, void *p3){
 
 void main(void){
     
-    	long t1 = 1, t2 = 2, t3 = 3, t4 = 4;
     
     k_thread_create(&t[0], tstacks[0], STACK_SIZE, random_gen,
-			INT_TO_POINTER(t1), NULL, NULL, 0, 0,
+			NULL, NULL, NULL, 0, 3,
 			K_NO_WAIT);
 
 	k_thread_create(&t[1], tstacks[1], STACK_SIZE, sha256,
-			INT_TO_POINTER(t2), NULL, NULL, 1, 0,
+			NULL, NULL, NULL, 0, 2,
 			K_NO_WAIT);
 
 	k_thread_create(&t[2], tstacks[2], STACK_SIZE, write_file,
-			INT_TO_POINTER(t3), NULL, NULL, 2, 0,
+			NULL, NULL, NULL, 0, 1,
 			K_NO_WAIT);
 			
     k_thread_create(&t[3], tstacks[3], STACK_SIZE, read_file,
-			INT_TO_POINTER(t4), NULL, NULL, 3, 0,
+			NULL, NULL, NULL, 0, 0,
 			K_NO_WAIT);
-	k_thread_resume(&t[0]);
-
+	k_thread_start(&t[0]);
+			
 }
